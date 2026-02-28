415304b508d368d8177e346fb4ccd536021d2ffb
Some_21 <skorpy.sanya@gmail.com>
2026-02-28 14:31:41 +0200

    h

---
+++ work_2.py
@@ -0,0 +1,12 @@
+class Car:
+    __init__(self, make, model, year):
+    self.make = make
+    self.model = model
+    self.year = year
+
+def get_info(self):
+    print(f"Ця машина {year} року, марка {make}, модель {model}")
+
+Car_1 = Car("Buggati", "FX-9000", "1990")
+
+get_info(Car_1)
