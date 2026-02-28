287408536720060246176e680f1dff20317ef380
Some_21 <skorpy.sanya@gmail.com>
2026-02-28 14:53:00 +0200

    j

---
+++ work_4.py
@@ -0,0 +1,15 @@
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+def calculate_area(self):
+    result = self.width * self.height
+    print(result)
+def calculate_perimetr(self):
+    result = 2*(self.width + self.height)
+    print(result)
+
+Pr_1 = Rectangle(100, 50)
+calculate_area(Pr_1)
+calculate_perimetr(Pr_1)
