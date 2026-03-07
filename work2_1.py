445cd051e70b8e8376b1476f082a0bacadd514a9
Some_21 <skorpy.sanya@gmail.com>
2026-03-07 15:58:40 +0200

    g

--- work2_1.py
+++ work2_1.py
@@ -11,18 +11,29 @@ class Cart:
 
     def add_product(self, products):
         self.spysok.append(products)
+       
 
     def Print_product(self):
-        print(f"Кошик продукців {self.coshic}")
+        print(f"Кошик покупця {self.coshic}")
         for i in self.spysok:
             print(i.name)
-            print(i.money)
+            print(i.money , "грн")
             print(i.actual)
 
-Product_1 = Product("eegs", 10, True)
+    def All_money(self):
+        result = 0
+        for i in self.spysok:
+            result += i.money 
+        print(f"Загальна вартість {result} грн")
+
+        
+
+Product_1 = Product("Яйця", 20, "Є в наявності")
+Product_2 = Product("Сир", 30, "Немає")
 cart = Cart("1")
-cart = Cart("2")
+
 
 cart.add_product(Product_1)
-
+cart.add_product(Product_2)
 cart.Print_product()
+cart.All_money()
--- work6.py
+++ work6.py
@@ -2,13 +2,15 @@ class LanguageProgram:
     def __init__(self):
         self.name = "Python"
     def print_Hello_world(self):
-        print("print(Hello world)")
+        print("print('Hello world')")
 
 class Java(LanguageProgram):
     def __init__(self):
         super().__init__()
         self.name = "Java"
-        
+    def print_Hello_world(self):
+        print("System.out.println('Hello, World!')")
+
 
 
 j = Java()
